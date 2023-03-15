e8803da76b99cb410bad675931abc7069d0475fe
baranner <github@kbaraniak.com>
2023-03-15 21:30:19 +0100

    awesome commit message

--- blog_updater.sh
+++ blog_updater.sh
@@ -13,6 +13,6 @@ if [ $NEW_SIZE != $LAST_SIZE ]; then
         ./jekyll_build.sh
 	cd _site
 	tar czf - * | ssh manitu "cd kbaraniak.com && tar xzf -"
-	python3 ../../telegram_messager/send_telegram_message.py breekon Kevin "Blog updated :)"
+	python3 /home/kev/telegram_messager/send_telegram_message.py breekon Kevin "Blog updated :)"
 fi
 
