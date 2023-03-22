9f56723c1c1fc4a4ce4e492e0847bbb56c2c5743
baranner <github@kbaraniak.com>
2023-03-22 16:16:48 +0100

    updated blog updater script

--- blog_updater.sh
+++ blog_updater.sh
@@ -1,17 +1,32 @@
 #! /bin/bash
 
-LAST_SIZE=$(du -sbc | grep -o '^[0-9]\+' | head -n 1)
-echo "Last size: $LAST_SIZE"
- 
-git pull
+echo -n "Checking repository..."
 
+LAST_SIZE=$(du -sbc | grep -o '^[0-9]\+' | head -n 1)
+git pull &>/dev/null
 NEW_SIZE=$(du -sbc | grep -o '^[0-9]\+' | head -n 1)
 
-echo "New size: $NEW_SIZE"
-
 if [ $NEW_SIZE != $LAST_SIZE ]; then
-        ./jekyll_build.sh
-	cd _site
-	tar czf - * | ssh manitu "cd kbaraniak.com && tar xzf -"
-	python3 /home/kev/telegram_messager/send_telegram_message.py breekon Kevin "Blog updated :)"
+        echo -n " found changes! rebuilding..."
+	./jekyll_build.sh &>/dev/null
+	RESULT=$?
+	
+	if [ $RESULT -eq 0 ]; then
+		echo -n " done! uploading..."
+		cd _site
+		tar czf - * | ssh manitu "cd kbaraniak.com && tar xzf -"
+		RESULT=$?
+		if [ $RESULT -eq 0 ]; then
+			echo " Done! Sending a telegram message and exiting then."
+			python3 /home/kev/telegram_messager/send_telegram_message.py breekon Kevin "Blog updated :)"
+		else
+			echo " Failed! :( exiting"
+			exit 1
+		fi
+	else
+		echo -n " Failed! :( exiting."
+		exit 1
+	fi
+else
+	echo " detected no changes, exiting."
 fi
